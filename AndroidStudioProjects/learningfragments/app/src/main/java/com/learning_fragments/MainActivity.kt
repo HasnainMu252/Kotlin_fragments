45852c26226a9f5fffa589873737b15b2af156b8
hasnain <hasnainmuneer@gmail.com>
2025-02-13 02:19:19 +0500

    i added the bottom menu

--- AndroidStudioProjects/learningfragments/app/src/main/java/com/learning_fragments/MainActivity.kt
+++ AndroidStudioProjects/learningfragments/app/src/main/java/com/learning_fragments/MainActivity.kt
@@ -4,36 +4,51 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
 import com.learning_fragments.databinding.ActivityMainBinding
-
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var fragmentManager: FragmentManager
-    private lateinit var binding : ActivityMainBinding
-
-
-
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
         binding = ActivityMainBinding.inflate(layoutInflater)
-
         setContentView(binding.root)
-        binding.fragment1.setOnClickListener{
-            gotoFragment(first_fragments())
+
+        val first = first_fragments()
+        val second = second_fragment()
+        val setting = setting()
+
+
+        // Set default fragment
+        setCurrentFragment(first)
+        //
+//        there is use of lamda -> use in the function
+//        we first make the menu in xml then we use id to to fetch the home , and shop and setting
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId){
+                R.id.home -> setCurrentFragment(first)
+                R.id.shop -> setCurrentFragment(second)
+                R.id.setting -> setCurrentFragment(setting)
+                else -> {
+                    setCurrentFragment(first)
+                }
+
+            }
+            true
         }
- binding.fragment2.setOnClickListener{
-            gotoFragment(second_fragment())
-        }
+//
+//
 
     }
 
-    private fun gotoFragment(fragment:Fragment){
-        fragmentManager = supportFragmentManager
-        fragmentManager.beginTransaction().replace(R.id.fragmentContainer,fragment).commit()
+    private fun setCurrentFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.FrameLayout, fragment)
+            addToBackStack(null)
+            commit()
+        }
     }
+
 }
